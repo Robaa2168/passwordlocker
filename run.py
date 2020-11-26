806cceec537035ff4a518c0b313e88d762d0064e
Robert Kipkoech <71884172+Robaa2168@users.noreply.github.com>
2020-11-26 08:41:41 +0300

    Update run.py
    edited delete user

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ def save_user(user):
     Function to save a new user
     '''
     user.save_user()
+
 def display_user():
     """
     Function to display  user
@@ -43,13 +44,6 @@ def display_accounts_details():
     Function that returns all the saved credential.
     """
     return Credentials.display_credentials()
-
-def delete_credential(credentials):
-    """
-    Function to delete a Credentials from credentials list
-
-    """
-    credentials.delete_credentials()
 
 def find_credential(account):
     """
