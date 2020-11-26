fe4e0eda75e299ef4b9d624c8e65c6b7f42db5f9
Robert Kipkoech <71884172+Robaa2168@users.noreply.github.com>
2020-11-26 07:58:23 +0300

    Update run.py
    new user function edits

--- run.py
+++ run.py
@@ -2,6 +2,12 @@
 from creditentials import User, Credentials
 
 
+def create_new_user(username,password):
+    '''
+    Function to create a new user 
+    '''
+    new_user = User(username,password)
+    return new_user
 
 def save_user(user):
     '''
